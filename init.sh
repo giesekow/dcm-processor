b4a974ab1f6536c45b96968af0e2e3755ac4e826
Giles Ekow Tetteh <giles.tetteh@tum.de>
2021-05-12 01:20:01 +0200

    fixed version of click

--- init.sh
+++ init.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-set -o allexport; source .env; set +o allexport
-
-[ -z "$BASEDIR" ] && echo "set BASEDIR variable in the .env file" && exit 1
-[ -z "$MODULES" ] && echo "set MODULES variable in the .env file" && exit 1
-[ -z "$REGISTRY" ] && echo "set REGISTRY variable in the .env file" && exit 1
-[ -z "$DATA" ] && echo "set DATA variable in the .env file" && exit 1
-[ -z "$LOGS" ] && echo "set LOGS variable in the .env file" && exit 1
-
-mkdir -p "$BASEDIR/$MODULES"
-mkdir -p "$BASEDIR/$REGISTRY"
-mkdir -p "$BASEDIR/$DATA"
-mkdir -p "$BASEDIR/$LOGS"
 
 bash service.sh install base -p ./services/base
 bash service.sh install dcm2nii -p ./services/dcm2nii
