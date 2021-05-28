c7c5d71acde23aa4b6586aa932baa99bce62ced9
Giles Ekow Tetteh <giles.tetteh@tum.de>
2021-05-28 13:36:13 +0200

    misc changes

--- containers/orthanc/DcmFilterLua.lua
+++ containers/orthanc/DcmFilterLua.lua
@@ -3,6 +3,9 @@ function ReceivedInstanceFilter(dicom, origin, info)
    -- Only allow incoming CT images
    if dicom.Modality == 'CT' then
    	return true
+   -- Only allow incoming MR images
+   elseif dicom.Modality == 'MR' then
+      return true
    -- Only allow incoming dcm-processed images
    elseif dicom.ActionSource ~= nil then
       return true
